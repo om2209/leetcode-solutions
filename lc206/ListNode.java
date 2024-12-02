c2bafd7c7b101250036c9936ad6bb14a9eb47bee
Omkar Mishra <omkarmishra66@gmail.com>
2024-12-02 15:01:36 +0530

    Added solutions for LC206

---
+++ lc206/ListNode.java
@@ -0,0 +1,23 @@
+package lc206;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
